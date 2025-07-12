6b7a1f0c1fb151b3740991ef8fb0f0b499749b89
anh.nguyen10 <@ucalgary.ca>
2025-07-12 16:34:13 -0600

    Updated catching error when parsing int

--- areaCalculator.py
+++ areaCalculator.py
@@ -15,33 +15,36 @@ typeMenu = """
 5. Parallelogram
 6. Trapezoid
 """
-
-print(typeMenu)
-typeUnit = int(input("Enter the shape in number: "))
-print(formula[typeUnit - 1])
-area = 0
-if typeUnit == 1:
-    length = float(input("Enter the length of side (number only): "))
-    area = length * length
-elif typeUnit == 2:
-    length = float(input("Enter the length of the rectangle (number only): "))
-    width = float(input("Enter the width of the rectangle (number only): "))
-    area = length * width
-elif typeUnit == 3:
-    base = float(input("Enter the base of the triangle (number only): "))
-    height = float(input("Enter the height of the triangle (number only): "))
-    area = base * height / 2
-elif typeUnit == 4:
-    radius = float(input("Enter the radius of the circle (number only): "))
-    area = (radius ** 2) * math.pi
-elif typeUnit == 5:
-    height = float(input("Enter the height of the parallelogram (number only): "))
-    base = float(input("Enter the base of the parallelogram (number only): "))
-    area = height * base
-elif typeUnit == 61:
-    height = float(input("Enter the height of the trapezoid (number only): "))
-    base = float (input("Enter the base of the trapezoid (number only): "))
-    length = float(input("Enter the parallel side of the base (number only):  "))
-    area = ((base * length)/2) * height
-print("{:.4f} unit²".format(area))
+try:
+    print(typeMenu)
+    typeUnit = int(input("Enter the shape in number: "))
+    print(formula[typeUnit - 1])
+    area = 0
+    if typeUnit == 1:
+        length = float(input("Enter the length of side (number only): "))
+        area = length * length
+    elif typeUnit == 2:
+        length = float(input("Enter the length of the rectangle (number only): "))
+        width = float(input("Enter the width of the rectangle (number only): "))
+        area = length * width
+    elif typeUnit == 3:
+        base = float(input("Enter the base of the triangle (number only): "))
+        height = float(input("Enter the height of the triangle (number only): "))
+        area = base * height / 2
+    elif typeUnit == 4:
+        radius = float(input("Enter the radius of the circle (number only): "))
+        area = (radius ** 2) * math.pi
+    elif typeUnit == 5:
+        height = float(input("Enter the height of the parallelogram (number only): "))
+        base = float(input("Enter the base of the parallelogram (number only): "))
+        area = height * base
+    elif typeUnit == 61:
+        height = float(input("Enter the height of the trapezoid (number only): "))
+        base = float (input("Enter the base of the trapezoid (number only): "))
+        length = float(input("Enter the parallel side of the base (number only):  "))
+        area = ((base * length)/2) * height
+    print("{:.4f} unit²".format(area))
+    
+except ValueError:
+    print("Error when taking inputs!")
 
